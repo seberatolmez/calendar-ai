7430b98b562ef3c0f5049ff76df00663976f27ec
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-10-26 20:17:30 +0300

    feat: improve login page uı

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,57 @@
 "use client";
-import {handleSignIn,handleSignOut} from "@/app/service/auth.service";
+import { handleSignIn, handleSignOut } from "@/app/service/auth.service";
 import { useSession } from "next-auth/react";
 
 export default function LoginPage() {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   console.log(session);
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Home</h1>
-      {session ? (
-        <button onClick={handleSignOut} 
-        className="bg-red-500 text-white px-4 py-2 rounded-md">
-          Sign Out
-        </button>
-      ) : (
-        <button onClick={handleSignIn} 
-        className="flex items-center !bg-[#6F55FF] text-white px-4 py-2 rounded-md">
-          <img loading="lazy" height="18" width="18" 
-          id="provider-logo-dark" 
-          src="https://authjs.dev/img/providers/google.svg"></img>
-          <span className="ml-2">Sign in with Google</span>
-        </button>
-      )}
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
+      <div className="bg-white shadow-lg rounded-2xl px-10 py-12 flex flex-col items-center space-y-6">
+        <div className="text-center space-y-2">
+          <h1 className="text-3xl font-semibold text-gray-800">
+            Log in to your account
+          </h1>
+          <p className="text-gray-500 text-sm">
+            Welcome back! Please log in to continue.
+          </p>
+        </div>
+
+        {session ? (
+          <button
+            onClick={handleSignOut}
+            className="bg-red-500 hover:bg-red-600 text-white text-base font-medium px-6 py-3 rounded-lg shadow-sm transition-all duration-200"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            onClick={handleSignIn}
+            className="flex items-center justify-center w-full gap-3 bg-[#6F55FF] hover:bg-[#5d46e0] text-white text-base font-medium px-7 py-3 rounded-lg shadow-md transition-all duration-200 cursor-pointer"
+          >
+            <img
+              loading="lazy"
+              height="24"
+              width="24"
+              id="provider-logo-dark"
+              src="https://authjs.dev/img/providers/google.svg"
+              alt="Google logo"
+            />
+            <span>Sign in with Google</span>
+          </button>
+        )}
+
+        <p className="text-gray-400 text-md pt-4">
+          Powered by{" "}
+          <a
+            href="https://gemini.google.com/app"
+            className="text-[#6F55FF] text-lg font-bold"
+            target="_blank"
+          >
+            Gemini
+          </a>
+        </p>
+      </div>
     </div>
   );
 }

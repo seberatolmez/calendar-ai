cf762d212a3294f47829bcd1868a8fe9c1270a15
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-10-26 21:38:28 +0300

    feat: implement footer into main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { LogOutIcon } from "lucide-react";
 export default function Home() {
     const {data: session} = useSession();
     const router = useRouter();
+    const developerEmail = process.env.NEXT_PUBLIC_DEVELOPER_EMAIL;
 
     useEffect(() => {
         if(!session || session === null) {
@@ -19,13 +20,20 @@ export default function Home() {
             <h1>Home</h1>
             <p>Welcome to the home page</p>
             <h1> Welcome back, {(session?.user?.name)}</h1>
-            <button className="bg-red-500 text-white px-4 py-2 rounded-md flex items-center gap-2" 
+            <button className="bg-red-500 text-white px-4 py-2 rounded-md flex items-center gap-2 cursor-pointer" 
             onClick={handleSignOut}
             disabled={!session || session === null}
             >
                 <LogOutIcon />
                 Sign Out
             </button>
+            <footer>
+                <p className="text-sm text-gray-500">Built by  
+                    <a href={`mailto:${developerEmail}`} 
+                    className="text-[#6F55FF] font-semibold hover:text-[#5d46e0]" 
+                    target="_blank"> Berat Ölmez</a></p>
+            </footer>
         </div>
+    
     )
 }

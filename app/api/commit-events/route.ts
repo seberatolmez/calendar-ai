dce66153293ed5e7a27fa00c661ee9627b26b5d2
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-10-29 12:09:59 +0300

    feat(api): implement empty routes to identify the architecture

--- app/api/commit-events/route.ts
+++ app/api/commit-events/route.ts
@@ -1,1 +1,8 @@
 // commit events to calendar using google calendar api
+
+import { NextRequest,NextResponse } from "next/server";       
+
+export async function Post( request: NextRequest) {
+
+
+}

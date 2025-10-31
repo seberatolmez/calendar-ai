741db4165ea721ae62b14f95db6408dffbf07d5d
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-10-31 16:57:35 +0300

    feat(api): implement api route for parsing events from prompt

--- app/api/parse-prompts/route.ts
+++ app/api/parse-prompts/route.ts
@@ -1,8 +1,26 @@
 // parse prompts from user and return parsed events
 
 import { NextRequest, NextResponse } from 'next/server';
+import { parseEventFromPrompt } from '@/app/service/ai.service';
 
 export async function POST(request: NextRequest) {
-
     
+    try {
+        const response = await parseEventFromPrompt( await request.text());
+        return NextResponse.json({
+            success: true,
+                response
+        }, {
+            status: 200
+        });
+
+    }catch (error) {
+        console.error('Error parsing prompts:', error);
+        return NextResponse.json({
+            success: false,
+            error: 'Failed to parse prompts'
+        }, {
+            status: 500
+        });
+    }
 }

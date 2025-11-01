676b23016c6dfa5b459ab3197e47a16325d174ee
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-11-01 11:07:51 +0300

    feat(api): implement route for commiting events to google calendar

--- app/api/commit-events/route.ts
+++ app/api/commit-events/route.ts
@@ -1,8 +1,47 @@
 // commit events to calendar using google calendar api
 
-import { NextRequest,NextResponse } from "next/server";       
+import { NextRequest,NextResponse } from "next/server";   
+import { createEvent } from "@/app/service/calendar.service";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function Post( request: NextRequest) {
 
+    const session = await getServerSession(authOptions);
 
+    if (!session || !session.accessToken) {
+    return NextResponse.json(
+      { error: 'Unauthorized' },
+      { status: 401 }
+        );
+    }
+
+    if (session.error) {
+    return NextResponse.json(
+      { error: 'Authentication expired, please sign in again' },
+      { status: 401 }
+        );
+    }
+
+    try {
+
+    const response = await createEvent(session.accessToken, await request.json()); // request body should be aligned with calendar_v3.Schema$Event
+            // todo: add validation for request body after creating proper type for event
+    return NextResponse.json( {
+        success: true,
+        response: response,
+        message: 'Event created successfully'
+    }, {
+        status: 200
+    });
+
+    } catch (error) {
+        console.error('Error committing event:', error);
+        return NextResponse.json({
+        success: false,
+        error: 'Failed to commit event',
+        }, 
+        { status: 500
+        });
+    }
 }

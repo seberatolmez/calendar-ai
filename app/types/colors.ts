39de2cdbcffc4c417fd0fd47385f1be154b5a828
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-11-09 19:45:05 +0300

    feat: rechange the dark-red color name to avoid misunderstanding

--- app/types/colors.ts
+++ app/types/colors.ts
@@ -5,13 +5,13 @@ export const COLORS = {
   1: { name: "blue", hex: "#a4bdfc" },
   2: { name: "green", hex: "#7ae7bf" },
   3: { name: "purple", hex: "#dbadff" },
-  4: { name: "red", hex: "#ff887c" },
+  4: { name: "light-red", hex: "#ff887c" },
   5: { name: "yellow", hex: "#fbd75b" },
   6: { name: "orange", hex: "#ffb878" },
   7: { name: "turquoise", hex: "#46d6db" },
   8: { name: "gray", hex: "#e1e1e1" },
   9: { name: "deep blue", hex: "#5484ed" },
   10: { name: "dark green", hex: "#51b749" },
-  11: { name: "dark red", hex: "#dc2127" },
-  
+  11: { name: "red", hex: "#dc2127" },
+
 }
